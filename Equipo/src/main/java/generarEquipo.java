355685cc460f5ae7a41de7ed79a22b689856824a
adrik1402 <74346933+adrik1402@users.noreply.github.com>
2021-01-30 12:34:09 +0100

    Add files via upload

---
+++ Equipo/src/main/java/generarEquipo.java
@@ -0,0 +1,22 @@
+package Equipo;
+        
+import java.util.Scanner;
+        
+public class generarEquipo extends Jugador{
+    
+         public String generarEquipo(){
+        String jugador;
+             jugador = this.mostrarJugador();
+        System.out.println("Cuantos jugadores contiene tu equipo?");
+            Scanner sc = new Scanner(System.in);
+            
+            
+             return jugador;
+            
+    }   
+        
+
+    }
+    
+    
+
