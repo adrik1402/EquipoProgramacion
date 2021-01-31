c35b36b829abc0062ff87e5c1974d52a603ae658
adrik1402 <74346933+adrik1402@users.noreply.github.com>
2021-01-31 12:28:26 +0100

    e puedo todo los metodos (estan vacios)

---
+++ EquipoProgra/src/equipoprogra/generarEquipo.java
@@ -0,0 +1,22 @@
+package equipoprogra;
+        
+import java.util.Scanner;
+        
+public class generarEquipo extends Jugador{
+    
+         public String generarEquipo(){
+        String jugador;
+             jugador = this.mostrarJugador();
+        System.out.println("Cuantos jugadores contiene tu equipo?");
+            Scanner sc = new Scanner(System.in);
+            
+            
+             return jugador;
+            
+    }   
+        
+
+    }
+    
+    
+
